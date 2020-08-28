415583fe349c3d915da66d4fd687681f30ae710e
Jonathan S <jonathan@videndum.studio>
2020-08-28 19:57:05 +0100

    Fixes Regex & adds Jest testing & adds Comment Support (#8)
    * feat: add support for issues and comments
    
    * feat: add support for issues and comments
    
    * fix: forgot to package xD
    
    * refactor: additional error context
    
    * Update .gitignore
    
    * style: change error text
    
    * Update .gitignore
    
    * fix: context not valid
    
    * refactor: add context to error
    
    * fix(package): PACKAGE YOU FOOL
    
    * fix: parse as json to error
    
    * fix(package): PACKAGE YOU FOOL
    
    * fix: issue_comment payload
    
    * feat: add jest & fixes regex
    
    * Fix?: jest
    
    * fix: workflow
    
    * feat: core.debug
    
    * feat(package): core.debug
    
    * feat: core.info instead of debug
    
    * fix: regex debuging
    
    * fix: define defaults
    
    * feat: support "where this issue was created"
    
    * Update main.yml
    
    * fix: ending at index 407
    
    * fix: fix title endpoint
    
    closes #6 & #7
    
    * Fix: minor title errors
    
    closes #8 #9 #11
    
    * Update main.yml
    
    * Update action.yml
    
    * fix: tests
    
    * part-revert: 3dcccbc15cf38054ac6c05c4326770081daf25e6
    
    * feat: adds pull_request_review support
    
    * docs: adds features
    
    The features I plan on developing xD
    
    * feat: jest coverage
    
    * fix: workflow running too much
    
    * fix: pull_request_review to use review content xD

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import * as github from '@actions/github'
+import * as helpers from './helpers'
 type options = {
   titlePrefix: string
   titleStart: string
@@ -8,6 +8,7 @@ type options = {
   bodyStart: string
   bodyEnd: string
   bodyEndRegex?: string
+  token: string
 }
 
 /**
@@ -18,173 +19,82 @@ type options = {
  * @since 2.0.0
  */
 async function run () {
-  try {
-    //@ts-ignore - Get the body of our PR so we can parse it
-    const prBody: string = github.context.payload.pull_request.body
-    const options: options = {
-      titlePrefix: core.getInput('title_prefix'),
-      titleStart: core.getInput('title_regex'),
-      titleEnd: core.getInput('title_end'),
-      bodyStart: core.getInput('body_start_regex'),
-      bodyEnd: core.getInput('body_end')
-    }
-    options.titleEndRegex = modeSwitch(
-      'title',
-      options.titleEnd,
-      options.titleStart
-    )
-    options.bodyEndRegex = modeSwitch(
-      'body',
-      options.bodyEnd,
-      options.bodyStart
-    )
+  const context: any = await helpers.getContext().catch(err => {
+    core.setFailed(err)
+  })
+  core.info('body: ' + context.body)
+  const options: options = {
+    titlePrefix: core.getInput('title_prefix') || '[DEBT]',
+    titleStart: core.getInput('title_regex') || '<!--\\[DEBT_ISSUE_TITLE\\]-->',
+    titleEnd: core.getInput('title_end') || 'line',
+    bodyStart:
+      core.getInput('body_start_regex') || '<!--\\[DEBT_BODY_START\\]-->',
+    bodyEnd: core.getInput('body_end') || 'paragraph',
+    token: core.getInput('token')
+  }
+  options.titleEndRegex = helpers.modeSwitch(
+    'title',
+    options.titleEnd,
+    options.titleStart
+  )
+  options.bodyEndRegex = helpers.modeSwitch(
+    'body',
+    options.bodyEnd,
+    options.bodyStart
+  )
 
-    /**
-     * Gets the content for title
-     * @author teepsdonnelly, TGTGamer
-     * @since 1.0.0
-     */
-    const debtIssueTitle = await parseContent(
-      prBody,
+  /**
+   * Gets the content for title
+   * @author teepsdonnelly, TGTGamer
+   * @since 1.0.0
+   */
+  const debtIssueTitle = (await helpers
+    .parseContent(
+      context.body,
       options.titleStart,
       options.titleEnd,
-      options.bodyEndRegex
-    ).catch(err => {
-      core.setFailed(err)
-    })
+      options.titleEndRegex
+    )
+    .catch(err => {
+      core.setFailed('Debt Title Error: ' + err)
+    })) as string
 
-    /**
-     * Gets the content for body
-     * @author teepsdonnelly, TGTGamer
-     * @since 1.0.0
-     */
-    const debtIssueBody = await parseContent(
-      prBody,
+  /**
+   * Gets the content for body
+   * @author teepsdonnelly, TGTGamer
+   * @since 1.0.0
+   */
+  const debtIssueBody = (await helpers
+    .parseContent(
+      context.body,
       options.bodyStart,
       options.bodyEnd,
       options.bodyEndRegex
-    ).catch(err => {
-      core.setFailed(err)
-      return ''
-    })
+    )
+    .catch(err => {
+      core.setFailed('Debt Body Error: ' + err)
+    })) as string
 
-    /**
-     * Creates the issue
-     * @author teepsdonnelly, TGTGamer
-     * @since 1.0.0
-     */
-    if (debtIssueTitle == '' || !debtIssueTitle) {
-      core.setFailed('You must at least provide a value for [DEBT_ISSUE_TITLE]')
-    } else {
-      createIssue(options.titlePrefix, debtIssueTitle, debtIssueBody)
-    }
-  } catch (error) {
-    core.setFailed(error.message)
+  /**
+   * Creates the issue
+   * @author teepsdonnelly, TGTGamer
+   * @since 1.0.0
+   */
+  if (debtIssueTitle == '' || !debtIssueTitle) {
+    core.setFailed('You must at least provide a value for the debt issue title')
+  } else {
+    helpers
+      .createIssue(
+        options.titlePrefix,
+        debtIssueTitle,
+        debtIssueBody,
+        context.html_url,
+        options.token
+      )
+      .catch(err => {
+        core.setFailed(err)
+      })
   }
-}
-
-/**
- * Switches the mode of the content endpoint
- * !TODO: Confirm this move worked before using
- * @author TGTGamer
- * @since 2.0.0
- */
-function modeSwitch (content: string, mode: string, StartRegex: any) {
-  switch (mode) {
-    case 'paragraph':
-      return `/${StartRegex}[^\\n]*\\.?/im`
-    case 'line':
-      return `/${StartRegex}[^.\\n]*\\.?/im`
-    case 'regex':
-      return `/${core.getInput(`${content}_end_regex`)}/im`
-    default:
-      return ''
-  }
-}
-
-/**
- * Look for a particular content in [STRING] format
- * @description Get the trimmed String after it up to the next new line
- * @author teepsdonnelly, TGTGamer
- * @returns String of content
- * @throws Error if regex can't be matched
- * @since 1.0.0
- */
-function parseContent (
-  body: string,
-  StartRegex: string,
-  endMode: string,
-  EndRegex: string
-): Promise<string> {
-  return new Promise(resolve => {
-    /**
-     * Finds the regex start point and gets the index number of it
-     * TODO: Cleanup because this ugly xD
-     * @author TGTGamer
-     * @since 2.0.0
-     */
-    const contentStart = body.match(`/${StartRegex}/im`)
-    if (!contentStart || !contentStart.index)
-      throw new Error('Start not matched')
-    const contentStartIndex = contentStart.index + contentStart.length
-
-    /**
-     * Finds the end point using `EndRegex`
-     * @author TGTGamer
-     * @since 2.0.0
-     */
-    const contentEnd = body.match(EndRegex)
-    if (!contentEnd || !contentEnd.index) throw new Error('End not matched')
-    var contentEndIndex: number
-    if (endMode == 'regex') {
-      contentEndIndex = contentEnd.index
-    } else {
-      contentEndIndex = contentEnd.index + contentEnd.length
-    }
-
-    /**
-     * Finds the content and checks its length.
-     * @author teepsdonnelly
-     * @since 1.0.0
-     */
-    const content = body.substring(contentStartIndex, contentEndIndex).trim()
-    if (content.length == 0) {
-      throw new Error('Content Length is 0')
-    }
-    resolve(content)
-  })
-}
-
-/**
- * Creates new issue
- * !TODO: Check that moving this hasn't affected functionality
- * @author teepsdonnelly
- * @since 1.0.0
- */
-async function createIssue (
-  titlePrefix: string,
-  issueTitle: string,
-  issueBody: string
-) {
-  const spacer = '\r\n\r\n'
-  const context = github.context
-  const myToken = core.getInput('token')
-  const octokit = new github.GitHub(myToken)
-  // @ts-ignore
-  const prHtmlURL = context.payload.pull_request.html_url
-
-  const newIssue = await octokit.issues.create({
-    ...context.repo,
-    title: titlePrefix + ' ' + issueTitle,
-    body:
-      issueBody +
-      spacer +
-      'See the [Pull Request that created this Issue](' +
-      prHtmlURL +
-      ')'
-  })
-
-  console.log('Issue created: ' + titlePrefix + ' ' + issueTitle)
 }
 
 run()
